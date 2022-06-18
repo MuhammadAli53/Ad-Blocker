eb2a0a28f779b36501f5ffabe4a7252f87f46a65
Muhammad Ali <lahore5310@gmail.com>
2022-06-18 11:19:25 +0500

    Ad Blocker App

---
+++ app/src/main/java/com/appwithali/adblocker/MainActivity.java
@@ -0,0 +1,27 @@
+package com.appwithali.adblocker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout tb;
+    ViewPager vp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tb=findViewById(R.id.tb);
+        vp=findViewById(R.id.vp);
+
+
+
+
+    }
+}
